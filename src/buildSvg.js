6c55cff0c6fd27e9678c2d227e52dc45ba75eb09
MKyrin <madjquirino@gmail.com>
2020-06-07 21:08:31 +0100

    Fractures image from input dir to output dir

---
+++ src/buildSvg.js
@@ -0,0 +1,90 @@
+import { getRandgomBetween } from './random.js'
+
+const MAX_NOISE = 10
+
+export const buildCellsSvgs = (diagram, width, height) => {
+    const endgeControlPoints = new Map();
+    const cellSvgs = {};
+
+    const buildCellSvg = (cell) => {
+        // let circles = ''
+        let fromPoint = getStartingPoint(cell)
+        let path = startPoint(fromPoint.x, fromPoint.y);
+        cell.halfedges.forEach(({ edge }) => {
+            const toPoint = getNextPoint(edge, fromPoint);
+            // console.log("EDGE: ", edge)
+            if (isEdgeOnFrame(edge, width, height)) {
+                path += lineTo(toPoint.x, toPoint.y)
+            }
+            else {
+                if (!endgeControlPoints.has(edge)) {
+                    endgeControlPoints.set(edge, getRandomControlPoint(edge))
+                };
+
+                // circles += `<circle cx="${endgeControlPoints.get(edge)[0]}" cy="${endgeControlPoints.get(edge)[1]}" r="5" stroke="blue" stroke-width="1" fill="blue" />`
+                path += curveTo(toPoint.x, toPoint.y, endgeControlPoints.get(edge)[0], endgeControlPoints.get(edge)[1])
+            }
+            fromPoint = toPoint;
+        });
+
+        return `<svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg">
+                    <path d="${path}" fill="black" stroke="transparent" />
+                </svg>`
+    }
+
+    diagram.cells.forEach(cell => {
+        cellSvgs[cell.site.voronoiId] = buildCellSvg(cell, width, height)
+        // console.log(`Cell SVG ${cell.site.voronoiId}`, cellSvgs[cell.site.voronoiId])
+    });
+    return cellSvgs
+}
+
+const startPoint = (x, y) => `M ${x} ${y}`
+
+const lineTo = (x, y) => ` L ${x} ${y}`
+
+const curveTo = (x, y, cx, cy) => ` Q ${cx} ${cy} ${x} ${y}`
+
+const getNextPoint = ({ va, vb }, previousPoint) =>
+    isSamePoint(va, previousPoint) ? vb : va;
+
+const getStartingPoint = (cell) => {
+    const { va: va1, vb: vb1 } = cell.halfedges[0].edge
+    const { va: va2, vb: vb2 } = cell.halfedges[cell.halfedges.length - 1].edge
+    if (isSamePoint(va1, va2) || isSamePoint(va1, vb2)) {
+        return va1
+    }
+    return vb1
+}
+
+const isSamePoint = (p1, p2) => p1.x === p2.x && p1.y === p2.y;
+
+const isEdgeOnFrame = ({ va: { x: x1, y: y1, }, vb: { x: x2, y: y2 } }, width, height) =>
+    (x1 === 0 && x1 === x2) ||
+    (x1 === width && x1 === x2) ||
+    (y1 === 0 && y1 === y2) ||
+    (y1 === height && y1 === y2)
+
+const getRandomControlPoint = ({ va: { x: x1, y: y1, }, vb: { x: x2, y: y2 } }) => {
+    const m = (y1 - y2) / (x1 - x2)
+    const b = y1 - (m * x1)
+
+    const dist = distanceBetweenPoints(x1, y1, x2, y2)
+
+    const rx1 = getRandgomBetween(Math.min(x1, x2) + dist / 5, Math.max(x1, x2) - dist / 5)
+    const ry1 = (m * rx1) + b;
+
+    const f = (minDistanceToClosestVertice(x1, y1, x2, y2, rx1, ry1) / getRandgomBetween(3,5));
+
+    const cx1 = (-(Math.sqrt((Math.pow(f, 2) * Math.pow(m, 2)) + Math.pow(f, 2)) / m) + (rx1 / Math.pow(m, 2)) + rx1) / ((1 / Math.pow(m, 2)) + 1)
+    const cy1 = (-cx1 + rx1) / m + ry1
+
+
+    return [cx1, cy1]
+}
+
+const minDistanceToClosestVertice = (x1, y1, x2, y2, cx, cy) =>
+    Math.min(distanceBetweenPoints(x1, y1, cx, cy), distanceBetweenPoints(x2, y2, cx, cy))
+
+const distanceBetweenPoints = (x1, y1, x2, y2) =>
+    Math.sqrt(Math.pow(Math.abs(x1 - x2), 2) + Math.pow(Math.abs(y1 - y2), 2))
--- src/main.js
+++ src/main.js
@@ -1,27 +1,95 @@
 import Voronoi from 'voronoi'
+import { buildCellsSvgs } from './buildSvg.js';
+import sharp from 'sharp'
+import rimraf from 'rimraf'
+import fs from 'fs'
+import { getRandomIntBetweem, getRandgomBetween } from './random.js';
 
-const Main = () => {
+const FIXED_SIZE = 800;
+
+const Main = async () => {
     const voronoi = new Voronoi();
-    const bbox = { xl: 0, xr: 800, yt: 0, yb: 800 };
-    const sites = [{ x: 200, y: 200 }, { x: 120, y: 15 },  { x: 75, y: 300 }, { x: 400, y: 100 }];
-    var diagram = voronoi.compute(sites, bbox);
+    const bbox = { xl: 0, xr: FIXED_SIZE, yt: 0, yb: FIXED_SIZE };
+    const files = fs.readdirSync('./data/in')
+    let fid = 0
+    let pairCount = 0
+   
+    rimraf.sync("./data/out");
 
-    console.log("Adjacents: ", getAdjacentSites(diagram.cells));
+    fs.mkdirSync(`./data/out/`);
+    fs.mkdirSync(`./data/out/pair`);
+    fs.mkdirSync(`./data/out/all`);
+
+
+    await asyncForEach(files, async file => {
+        if (file.includes('.png') || file.includes('.jpg')) {
+            console.log('Fracturing: ', file)
+            const sites = getRandomSites()
+            const diagram = voronoi.compute(sites, bbox)
+            const cellSvgs = buildCellsSvgs(diagram, FIXED_SIZE, FIXED_SIZE)
+            const adjacents = getAdjacentSites(diagram.cells)
+
+            await asyncForEach(diagram.cells, async cell => {
+                const siteId = cell.site.voronoiId
+                const svg = cellSvgs[siteId]
+                const shape = Buffer.from(svg)
+                try {
+                    await sharp(`./data/in/${file}`)
+                        .resize({ width: FIXED_SIZE, height: FIXED_SIZE, options: { fit: 'contain' } })
+                        .composite([{
+                            input: shape,
+                            blend: 'dest-in'
+                        }])
+                        .jpeg()
+                        .toFile(`./data/out/all/${fid}_${siteId}.jpg`)
+                } catch (writeErr) {
+                    console.log("Error writing file: ", writeErr)
+                }
+            })
+
+            adjacents.forEach(async pair => {
+                const s1 = pair[0]
+                const s2 = pair[1]
+                try {
+                    fs.mkdirSync(`./data/out/pair/${pairCount}`)
+
+                    fs.copyFileSync(`./data/out/all/${fid}_${s1}.jpg`, `./data/out/pair/${pairCount}/${fid}_${s1}.jpg`)
+                    fs.copyFileSync(`./data/out/all/${fid}_${s2}.jpg`, `./data/out/pair/${pairCount}/${fid}_${s2}.jpg`)
+                    pairCount++;
+                } catch (writeErr) {
+                    console.log("Error writing file: ", writeErr)
+                }
+            })
+
+            voronoi.recycle(diagram)
+            fid++
+        }
+    })
+}
+
+
+const getRandomSites = () => {
+    const sites = []
+    const nSites = getRandomIntBetweem(2, 6)
+    for (let i = 0; i < nSites; i++) {
+        sites.push({ x: getRandgomBetween(0, FIXED_SIZE), y: getRandgomBetween(0, FIXED_SIZE) })
+    }
+    return sites;
 }
 
 const getAdjacentSites = cells => {
     let adjacentSites = [];
-    let testedSites = [];    
+    let testedSites = [];
     cells.forEach(cell1 => {
         cells.forEach(cell2 => {
-            if(cell1 !== cell2) {
+            if (cell1 !== cell2) {
                 let pairId = makeCellPairId(cell1, cell2);
-                if(!containsPairId(testedSites, pairId) && !containsPairId(adjacentSites, pairId)) {
-                    if(isAdjacentCell(cell1, cell2)){
+                if (!containsPairId(testedSites, pairId) && !containsPairId(adjacentSites, pairId)) {
+                    if (isAdjacentCell(cell1, cell2)) {
                         adjacentSites.push(pairId);
                     }
                     testedSites.push(pairId);
-                }                
+                }
             }
         });
     });
@@ -34,8 +102,14 @@ const makeCellPairId = (cell1, cell2) =>
 const containsPairId = (array, pair) =>
     array.some(elm => elm[0] === pair[0] && elm[1] === pair[1]);
 
-const isAdjacentCell = (cell1, cell2) => 
-    cell1.halfedges.some(edge1 => 
+const isAdjacentCell = (cell1, cell2) =>
+    cell1.halfedges.some(edge1 =>
         cell2.halfedges.some(edge2 => edge1.edge === edge2.edge));
+
+const asyncForEach = async (array, callback) => {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
 
 export default Main
